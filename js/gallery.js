0da1f89e78c4a1203e2ef162bf9bfa0a3c97defa
Vlad Buchkovskiy <buchkek@gmail.com>
2021-04-11 14:42:58 +0300

    Web site is done. Masonry bad, need to finish up

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+// var elem = document.querySelector('.grid');
+// var msnry = new Masonry( elem, {
+//     // options
+//     itemSelector: '.grid-item',
+//     columnWidth: 200,
+//     gutter: 0
+// });
+
+
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    gutter: 20
+});
